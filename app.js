5f9512b53d3c2434a59ab8aaa8e9e69c3e29e1e3
tonmoy <tonmoy3673@gmail.com>
2025-03-14 13:45:10 +0600

    :bulb: set voice converter function

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('connected');
+// ======== voice converter function =========//
+let speech =new SpeechSynthesisUtterance();
+document.getElementById('btn').addEventListener('click',function(){
+speech.text = document.querySelector('textarea').value;
+window.speechSynthesis.speak(speech);
+console.log(speech);
+});
